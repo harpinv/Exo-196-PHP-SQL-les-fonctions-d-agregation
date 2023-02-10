1b9387d629a09baca71aecc90ff74dc94d7a7495
vince <vincent.harpin@orange.fr>
2023-02-10 16:30:59 +0100

    exercice 196

--- index.php
+++ index.php
@@ -25,6 +25,92 @@
 
     // TODO Votre code ici, commencez par require un des objet de connexion que nous avons fait ensemble.
 
+    $server = 'localhost';
+    $user = 'root';
+    $pwd = '';
+    $db = 'bdd_cours';
+
+    try {
+
+        $maConnexion = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pwd);
+        $maConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $maConnexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        //question 3
+        $pom = $maConnexion->prepare("SELECT MIN(age) as minimum FROM user");
+
+        $state = $pom->execute();
+
+        if ($state) {
+            $min = $pom->fetch();
+            echo "<div>Le plus petit age trouver est: " . $min['minimum'] . " ans</div>";
+        } else {
+            echo "Une erreur est survenue en récupérant les données de la table.";
+        }
+
+        //question 4
+        $pom = $maConnexion->prepare("SELECT MAX(age) as maximum FROM user");
+
+        $state = $pom->execute();
+
+        if ($state) {
+            $max = $pom->fetch();
+            echo "<div>Le plus grand age trouver est: " . $max['maximum'] . " ans</div>";
+        } else {
+            echo "Une erreur est survenue en récupérant les données de la table.";
+        }
+
+        //question 5
+        $pom = $maConnexion->prepare("SELECT count(*) as number FROM user WHERE nom");
+
+        $state = $pom->execute();
+
+        if ($state) {
+            $count = $pom->fetch();
+            echo "<div>Le nombre d'utilisateur est: " . $count['number'] . "</div>";
+        } else {
+            echo "Une erreur est survenue en récupérant les données de la table.";
+        }
+
+        //question 6
+        $pom = $maConnexion->prepare("SELECT count(*) as number FROM user WHERE numero >= 5");
+
+        $state = $pom->execute();
+
+        if ($state) {
+            $count = $pom->fetch();
+            echo "<div>Le nombre d'utilisateur ayant un numero de rue supérieur ou égale à cinq est: " . $count['number'] . "</div>";
+        } else {
+            echo "Une erreur est survenue en récupérant les données de la table.";
+        }
+
+        //question 7
+        $pom = $maConnexion->prepare("SELECT AVG(age) as moyenne_age FROM user");
+
+        $state = $pom->execute();
+
+        if ($state) {
+            $average = $pom->fetch();
+            echo "<div>L'age moyen des utilisateurs est: " . $average['moyenne_age'] . " ans</div>";
+        } else {
+            echo "Une erreur est survenue en récupérant les données de la table.";
+        }
+
+        //question 8
+        $pom = $maConnexion->prepare("SELECT SUM(numero) as somme_numero FROM user");
+
+        $state = $pom->execute();
+
+        if ($state) {
+            $sum = $pom->fetch();
+            echo "<div>La somme des numéro de maison des utilisateurs est: " . $sum['somme_numero'] . "</div>";
+        } else {
+            echo "Une erreur est survenue en récupérant les données de la table.";
+        }
+    }
+    catch (PDOException $exception) {
+        echo "Erreur de connexion: " . $exception->getMessage();
+    }
 
     ?>
 </body>
